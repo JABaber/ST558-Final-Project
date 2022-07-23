3de8731b745725e77b04eb0e2d5741026122fd8a
Joshua Baber <joshuaababer@gmail.com>
2022-07-23 11:15:38 -0400

    Implemented shinydashboard on UI side with histogram example

--- DiscGolfApp/ui.R
+++ DiscGolfApp/ui.R
@@ -1,33 +1,48 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# ST558 Final Project (UI Side)
+# Josh Baber
+# shiny::runGitHub("JABaber/ST558-Final-Project", subdir = "DiscGolfApp/")
 
 library(shiny)
+library(shinydashboard)
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Example", tabName = "exampleSection"),
+    menuItem("About", tabName = "aboutSection"),
+    menuItem("Data Exploration", tabName = "EDASection"),
+    menuItem("Modeling Info", tabName = "modelingInfoSection"),
+    menuItem("Model Fitting", tabName = "modelFittingSection"),
+    menuItem("Prediction", tabName = "predictionSection"),
+    menuItem("Data", tabName = "dataSection")
+  )
+)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+body <- dashboardBody(
+    tabItems(
+      tabItem(tabName = "exampleSection",
+              fluidRow(
+                box(
+                    "Box content here", br(), "More box content",
+                    sliderInput("bins",
+                                "Number of bins:",
+                                min = 1,
+                                max = 50,
+                                value = 30),
+                    textInput("test", "Text Input:")
+                ),
+    
+                box(
+                    plotOutput("distPlot")
+                )
+              )
+      )
     )
-))
+)
+
+dashboardPage(
+  skin = "yellow",
+  dashboardHeader(title = "Example Histogram"),
+  sidebar,
+  body
+)
